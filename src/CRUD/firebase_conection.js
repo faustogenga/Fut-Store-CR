887edc1291a043a07117dfd7075d2f590196daaa
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2023-11-27 12:25:27 -0600

    update firebase key to original

--- src/CRUD/firebase_conection.js
+++ src/CRUD/firebase_conection.js
@@ -4,13 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBDDDQ9ai8qmiE_7qUloj1a5q54Gr1fZk",
-  authDomain: "dwii-fb3da.firebaseapp.com",
-  projectId: "dwii-fb3da",
-  storageBucket: "dwii-fb3da.appspot.com",
-  messagingSenderId: "300929602559",
-  appId: "1:300929602559:web:19979c0d4a964e7d3a8dca",
-  measurementId: "G-LLPXQKJZW7"
+  apiKey: "AIzaSyC-Aa-blH9dsUfJ9cZMKQeU4DXyNbAUvT0",
+  authDomain: "dw---base-de-datos.firebaseapp.com",
+  projectId: "dw---base-de-datos",
+  storageBucket: "dw---base-de-datos.appspot.com",
+  messagingSenderId: "692244881069",
+  appId: "1:692244881069:web:123bc7b8ac6fe45d98f024"
 };
 
 // Initialize Firebase
