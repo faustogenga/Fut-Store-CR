e47f2228755360d31c0bfe1b37cbbbc33d10094b
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2023-12-21 16:15:00 -0600

    efectos al home

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 
-export const Banner = ({isVendor}) => {
+export const Banner = ({ isVendor }) => {
 
     const navigate = useNavigate();
 
     return (
-        <div className='container-fluid mx-auto d-flex justify-content-center align-items-center' style={{ height: '300px', backgroundColor: '#f4f4f4' }}>
-            <div className='col-8 mx-auto bg-white text-center' style={{ height: '80%' }}>
+        <div className='container-fluid mx-auto d-flex justify-content-center align-items-center' style={{ height: '400px', backgroundColor: '#e3e3e3' }}>
+            <div className='col-8 mx-auto bg-white text-center d-flex flex-column justify-content-center align-items-center' style={{ height: '80%' }}>
                 <h3 className='m-4'>Vende tus Articulos con Nosotros</h3>
                 <div className='m-3'>
                     <blockquote className="blockquote">
                         <p className="mb-0">Tus productos en nuestra pagina a solo un click</p>
                     </blockquote>
-                    <button type="button"  disabled={isVendor ? true : false} className="btn btn-info m-4" onClick={()=> (navigate('/NewVendor'))}>Inscribete</button>
+                    <div className='flex-column'>
+                        <div className=''>
+                            <img
+                                style={{
+                                    width: "10%",
+                                    position: "relative",
+                                    left: "60px",
+                                    top: "10px",
+                                    opacity:"0.8"
+                                }}
+                                alt="soccerball" 
+                                src="https://media0.giphy.com/media/Lm5hxmmI6ucOQGfjKj/giphy.gif?cid=ecf05e47yzkw71pcxrsmp94cn2lc79qwbg257vfepgvrzx9r&ep=v1_stickers_search&rid=giphy.gif&ct=s" />
+                        </div>
+                        <div>
+                            <button type="button" disabled={isVendor ? true : false} className="btn btn-info" onClick={() => (navigate('/NewVendor'))}>Inscribete</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,22 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player/youtube'
+import '../CSS/Video.css'
 
 export const Video = ({ isVendor }) => {
 
-    return (
-        <div className='container-fluid d-flex justify-content-center align-items-center mt-1 mb-1 bg-black' style={{ height: '600px' }}>
-            <div className='col-10 bg-white d-flex justify-content-center' style={{ height: '90%' }}>
-                <ReactPlayer 
-                width={"75%"} 
-                height={"99%"} 
-                playing="true"
-                loop="true"
-                volume={0}
+    const [scrollPosition, setScrollPosition] = useState(0);
 
-                url='https://www.youtube.com/watch?v=eGUor824a74&ab_channel=SportsOnScreen' 
-                />
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position / 100);
+    };
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <>
+            <div className='container-fluid d-flex mt-2 bg-black' style={{ height: '650px' }}>
+                <div className='col-10 d-flex justify-content-center align-items-center p-4' style={{ height: '100%' }}>
+                    <ReactPlayer
+                        width={"90%"}
+                        height={"100%"}
+                        playing="true"
+                        loop="true"
+                        volume={0}
+
+                        url='https://www.youtube.com/watch?v=eGUor824a74&ab_channel=SportsOnScreen'
+                    />
+                </div>
+                <div className='col-2 overflow-hidden mt-3'>
+                    <div className=''>
+                        <img
+                            className='rounded m-2'
+                            alt="futbol"
+                            width={"82%"}
+                            style={{ transform: `rotate(${scrollPosition - 13}deg)` }}
+                            src='https://i.pinimg.com/564x/6c/cd/60/6ccd60c6920ce2041ec084b312614fee.jpg' />
+                        <img
+                            className='rounded m-2'
+                            alt="futbol"
+                            width={"82%"}
+                            height={"25%"}
+                            style={{ transform: `rotate(-${scrollPosition - 13}deg)` }}
+                            src='https://i.pinimg.com/564x/07/76/da/0776daded68920d37af6e46d08e989ba.jpg' />
+                        <img
+                            className='rounded m-2'
+                            alt="futbol"
+                            width={"82%"}
+                            height={"25%"}
+                            style={{ transform: `rotate(${scrollPosition - 13}deg)` }}
+                            src='https://i.pinimg.com/564x/68/00/90/680090c1960a48f53f8cbc9778db33e8.jpg' />
+                        <img
+                            className='rounded m-2'
+                            alt="futbol"
+                            width={"82%"}
+                            height={"25%"}
+                            style={{ transform: `rotate(-${scrollPosition - 13}deg)` }}
+                            src='https://i.pinimg.com/564x/77/2e/80/772e80a8b1683dfa0b59846253f09cf1.jpg' />
+
+                    </div>
+                </div>
             </div>
-            <div className='col-2'>Hola</div>
-        </div>
+            <div className='container-fluid d-flex bg-black' style={{marginBottom: "1px"}}>
+                <div className='col-1 mx-3 p-1' style={{ color: "white", fontFamily: "Oswald", fontWeight:"700" }}>FUTSTORE cr</div>
+                <div className='col-9 mx-3 p-1 d-flex overflow-hidden'>
+                    <div className='texto1 text-nowrap'>
+                        NIKE - ADIDAS - PUMA - UNDER ARMOUR - NEW BALANCE - MIZUNO - UMBRO - DIADORA - LOTTO - KAPPA -
+                        REEBOK - ASICS - JOMA - UHLSPORT - HUMMEL - ERREA - CANTERBURY - KELME - PENALTY - LECOQSPORTIF -
+                        MOLTENI - WARRIOR - XARA - AVIA - PEAK - YONEX - MITRE - VOIT - GOLA - CUERVO - STANNO  -
+                    </div>
+                    <div className='texto2 text-nowrap'>
+                        NIKE - ADIDAS - PUMA - UNDER ARMOUR - NEW BALANCE - MIZUNO - UMBRO - DIADORA - LOTTO - KAPPA -
+                        REEBOK - ASICS - JOMA - UHLSPORT - HUMMEL - ERREA - CANTERBURY - KELME - PENALTY - LECOQSPORTIF -
+                        MOLTENI - WARRIOR - XARA - AVIA - PEAK - YONEX - MITRE - VOIT - GOLA - CUERVO - STANNO  -
+                    </div>
+                </div>
+                <div className='col-1 mx-3 p-1' style={{ color: "white", fontFamily: "Oswald", fontWeight:"700"  }}>FUTSTORE cr</div>
+            </div>
+        </>
     )
 }
