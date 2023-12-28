cd7e9205864b6e212a90b160faa626c324df6755
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2023-12-27 18:56:09 -0600

    updates

--- src/components/Productitem.jsx
+++ src/components/Productitem.jsx
@@ -1,4 +1,4 @@
-import Reac from 'react'
+import React from 'react'
 import { useNavigate } from "react-router-dom";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import IconButton from '@mui/material/IconButton';
@@ -24,7 +24,7 @@ export const Productitem = ({ loggedIn, product, isVendor }) => {
       >
       </img>
       <div className='descripcion m-1'>
-        <h5>{product.name}</h5>
+        <small>{product.name}</small>
         <p className='m-0'><strong>${product.price}</strong></p>
         <IconButton disabled={isVendor ? true : false} color="primary" aria-label="add to shopping cart"
           onClick={() => (loggedIn ? onButtonClickViewProduct() : Swal.fire({
